18d6d5786faba7268bad8589bd669d25b81edf96
WISDOMVAST <WISDOMVAST@DESKTOP-LK4MLAB>
2020-07-06 14:15:11 +0700

    add new test case web

---
+++ Test Suites/Test Web.ts
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Test Web</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>db874c07-4057-4795-8ee0-bbe08491d2b2</testSuiteGuid>
+   <testCaseLink>
+      <guid>37dfeef4-45e0-4a9f-850c-2418f093af31</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/New Test Case Web</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
